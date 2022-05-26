fbc316540fed351649d15cf3885aef119bc51fea
alick.shi <shi_xinlei@qq.com>
2022-05-26 17:17:17 +0800

    增加 Full Sceen 类

--- src/utils/FullScreen.ts
+++ src/utils/FullScreen.ts
@@ -1,52 +1,47 @@
 
-
+interface ToggleFull { success?: (str: string) => void; error?: (str: string) => void; }
 class FullScreen {
     public isFullScreen: boolean;
-    public loading: boolean;
     private container: any;
-
 
     constructor({ container }: any) {
         this.isFullScreen = false;
-        this.loading = false;
         this.container = container
     }
 
-
     //进入全屏
-    toggleFullscreen () {
+    public toggleFullscreen (callback?: ToggleFull) {
         let elem = this.container;
-
         elem.onfullscreenchange = this.handleFullscreenChange;
         if (!document.fullscreenElement) {
             // @ts-ignore
             if (elem.requestFullscreen) {
-                elem.requestFullscreen().then({}).catch((err: any) => {
-                    alert(`启用全屏出现错误: ${err.message} (${err.name})`);
+                elem.requestFullscreen().then(() => {
+                    callback && callback.success && callback.success("打开全屏成功");
+                }).catch((err: Error) => {
+                    console.warn(`启用全屏出现错误: ${err.message} (${err.name})`);
+                    callback && callback.error && callback.error(`启用全屏出现错误: ${err.message} (${err.name})`);
                 });
             } else {
-                console.error("浏览器不支持全屏");
+                console.warn("浏览器不支持全屏");
+                callback && callback.error && callback.error("浏览器不支持全屏");
             }
 
         } else {
-            document.exitFullscreen().then(() => new Error("全屏模式退出"))
+            document.exitFullscreen().then(() => {
+                callback && callback.success && callback.success("退出全屏成功");
+            })
                 .catch((err) => new Error(err));
         }
-    };
-    //退出全屏
-    exitFullscreen = () => {
-       this.toggleFullscreen();
-    };
+    }
+   
 
     private handleFullscreenChange (e: any) {
-        this.loading = true;
         let elem = e.target;
-        let isFullscreen = document.fullscreenElement === elem;
-        this.isFullScreen = isFullscreen;
-        setTimeout(() => {
-            this.loading = false;
-        }, 300);
+        this.isFullScreen = document.fullscreenElement === elem;
     }
+
+
 }
 
 export default FullScreen;
